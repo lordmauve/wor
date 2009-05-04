1dafa96b7f3834a7a00436bdd8f51baefd836ec5
hrm <hrm@localhost>
2009-05-04 23:04:51 +0000

    Start to implement Items -- incomplete

--- library/Item.py
+++ library/Item.py
@@ -4,57 +4,66 @@
 # represent a block of otherwise identical things -- e.g. coins -- and
 # can be combined and split (see AggregateItem).
 
-class Item(object):
-    def __init__(self):
-        pass
+import SerObject
+import Actor
+import Location
+import Util
 
-    def load(id):
-        """Get the item with the given id"""
-        # FIXME: Caching here
-        try:
-            cur = DB.db.cursor()
-            cur.execute('SELECT state FROM item'
-                        + ' WHERE id=%(id)s',
-                        { 'id': id }
-                        )
-            row = cur.fetchone()
-            obj = pickle.loads(row[0])
-            obj._changed = False
-            return obj
-        except:
-            return None
+class Item(SerObject.SerObject):
+    # Failure-rate (aka hazard) functions: return the probability
+    # [0.0, 1.0) that this item will fail, given a lifetime of x
+    @staticmethod
+    def weibull(x, k, l):
+        """Weibull failure function, with position l and shape k.  The
+        mean of this distribution is l*GAMMA(1+1/k), where GAMMA is
+        the gamma function."""
+        return k/l * math.pow(x/l, k-1.0)
 
-    def save(self):
-        if not self._changed:
-            return
+    # Default failure function is a mean life of 300, and equal
+    # probability of failing on each use.
+    break_profile = lambda life: weibull(life, 300.0, 1.0)
 
-        cur = DB.db.cursor()
-        cur.execute('UPDATE ' + table
-                    + ' SET state = %(state)s WHERE id = %(id)s',
-                    { 'id': self.id,
-                      'state': pickle.dumps(self) }
-                    )
+    @staticmethod
+    def name_for(player):
+        return "Item"
 
-        self._changed = False
-
-    def __init__(self, id):
-        """Create a completely new actor"""
-        self.id = id
-        self.state = {}
+    @staticmethod
+    def plural_for(player):
+        return name_for(player) + 's'
 
     ####
-    # Pickling (serialisation)
-    def __getstate__(self):
-        return self.state
+    # Add the indices for saving this object
+    def _save_indices(self):
+        return { 'owner_type': self.owner_type,
+                 'owner': self.owner,
+                 'count': self.count,
+                 'type': self.type }
+    
+    def owner(self):
+        """Return the owner of this object, loading it if necessary"""
+        if self._owner == None:
+            if self.owner_type == 'Actor':
+                self._owner = Actor.load(self.owner)
+            elif self.owner_type == 'Location':
+                self._owner = Location.load(self.owner)
+            elif self.owner_type == 'Item':
+                self._owner = Item.load(self.owner)
+            else:
+                pass
+        return self._owner
 
-    def __setstate__(self, state):
-        self.state = state
-        self._container = None
+    def power(self, name):
+        return Util.default(self[name])
 
-    ####
-    # Basic properties
-    def container(self):
-        # A container can be an actor, another item (bag, pack-horse),
-        # or a location.
-        if self._container = None:
-            pass
+    def destroy(self):
+        """Destroy this item, recycling it if necessary"""
+        self.demolish()
+
+    def try_break(self):
+        """Test this item for breakage, and return True if it broke"""
+        if random.random() < self.break_profile(self.lifetime):
+            # Then we broke. You may cry.
+            self.destroy()
+            return True
+
+        return False
