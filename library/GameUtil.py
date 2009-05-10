cca37072ea38ddf5d2d11fe4525ebeb20a61c491
hrm <hrm@localhost>
2009-05-10 12:26:37 +0000

    Add basic management tools for locations.
    Add Location cache to GameUtil.save()

--- library/GameUtil.py
+++ library/GameUtil.py
@@ -2,9 +2,11 @@
 
 from Item import Item
 from Player import Player
+from Location import Location
 
 def save():
 	"""Save all first-class object caches"""
 	Item.save_cache()
 	Player.save_cache()
-	#Location.save_cache()
+	#NPC.save_cache()
+	Location.save_cache()
