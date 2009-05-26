84e909468a3cf4d9fd09c7e272da0738d74922a5
hrm <hrm@localhost>
2009-05-26 20:01:52 +0000

    Change "actor_id" column in actor table to "id", to fit with SerObject code (and the design style of the rest of the DB schema).
    Remove NOT NULL constraints on location (x,y,layer) index fields.
    
    Add (x,y,layer) index fields on actors.
    
    As a result of Sert's schema changes in r1630:1632, we will be storing
    all actors in the same table. This necessitates some changes to the
    Actor/Player code. The main by-id cache is moved up the hierarchy, and
    is now part of Actor. The by-name cache remains in Player, as only
    Players have names.
    
    Also add position indexing for all Actors, make a position a required
    field in an Actor's constructor, and tweak the Player constructor to
    handle it, too (with a note to fill in some code to make it alignment
    dependent).

--- library/Player.py
+++ library/Player.py
@@ -7,12 +7,11 @@ from ItemContainer import ItemContainer
 from Punch import Punch
 from SimpleAction import SimpleAction
 from SimpleTimedCounter import SimpleTimedCounter
+from Position import Position
 from Logger import log
 
 class Player(Actor.Actor):
-	# We have our own DB table and caching scheme for Players
-	_table = 'player'
-	cache_by_id = {}
+	# Additional caching over and above the cache by ID for actors
 	cache_by_name = {}
 
 	####
@@ -26,7 +25,7 @@ class Player(Actor.Actor):
 
 		cur = DB.cursor()
 		cur.execute("SELECT id FROM " + cls._table
-					+ " WHERE username = %(name)s",
+					+ " WHERE name = %(name)s",
 					{ 'name': name } )
 		row = cur.fetchone()
 		if row == None:
@@ -39,19 +38,24 @@ class Player(Actor.Actor):
 		"""Hook to allow classes to define their own caching scheme.
 		Called after an object has been loaded and cached in
 		cls.cache_by_id"""
+		super(Player, cls)._cache_object(obj)
 		cls.cache_by_name[obj.name] = obj
 
 	####
 	# Additional indices to write to the database on save
 	def _save_indices(self):
 		inds = super(Player, self)._save_indices()
-		inds['username'] = self.name
+		inds['name'] = self.name
 		return inds
 
 	####
 	# Create a new player
-	def __init__(self, name, password, align):
-		super(Player, self).__init__()
+	def __init__(self, name, align, position=None):
+		if position == None:
+			# FIXME: Make this alignment-dependent
+			position = Position(0, 0, 'main')
+
+		super(Player, self).__init__(name, position)
 		self.align = align
 		# Start at 120AP, get one more every 6 minutes (240 per day),
 		# maximum 240.
@@ -59,12 +63,12 @@ class Player(Actor.Actor):
 		self.ap = SimpleTimedCounter(self, 120, 360, 240)
 		self.hp = 300
 		self.maxhp = 300
-		self.name = name
 		self.inventory = ItemContainer(self, "inventory")
 		punch = Punch()
 		self.inventory.add(punch)
 
-		self._cache_object(self) # Must be called explicitly in __init__()
+		self._cache_object(self) # Must be called explicitly in
+                                 # __init__() here.
 
 	####
 	# Movement
