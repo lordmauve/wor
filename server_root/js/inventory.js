8a906d62ef67ffd8660af955f12b4e033d1ebf31
hrm <hrm@localhost>
2009-06-19 23:29:23 +0000

    Add sketch of UI for changing the held item.

---
+++ server_root/js/inventory.js
@@ -0,0 +1,71 @@
+// Inventory handling
+
+// Start to select an item from the player's inventory
+function show_items()
+{
+	var select_panel = document.getElementById("inventory_panel");
+	select_panel.innerHTML = "<table id='inventory_panel_table'></table>";
+	select_panel.visible = true;
+
+	var account = document.getElementById("account").value
+	var password = document.getElementById("password").value
+	var actid = document.getElementById("actorid").value
+
+	var req = get_ajax_object();
+	req.onreadystatechange = function() { inventory_response(req); }
+	req.open("GET", api + "/actor/self/inventory", true, account, password);
+	req.setRequestHeader("X-WoR-Actor", actid);
+
+	req.send("");
+}
+
+function inventory_response(req)
+{
+	if(req.readyState == 4)
+	{
+		var select_table = document.getElementById("inventory_panel_table");
+		if(req.status == 200)
+		{
+			// Get the table of item data
+			var items = parse_input_table(req.responseText, 3);
+			items.sort(function(a,b) {
+						if(a[2] == b[2]) return 0;
+						if(a[2] < b[2]) return -1;
+						return 1;
+					});
+
+			nlines = Math.floor((items.length-1)/3) + 1;
+			for(var i=0; i<nlines; i++)
+			{
+				var line = "<tr>";
+				for(var j=0; j<3; j++)
+				{
+					line += "<td>";
+					pos = j*nlines + i;
+					if(pos < items.length)
+					{
+						line += "<a href='javascript:inventory_change(";
+						line += items[pos][1];
+						line += ")'>" + items[pos][2] + "</a>";
+					}
+					line += "</td>";
+				}
+				line += "</tr>";
+				select_table.innerHTML += line;
+			}
+		}
+		else
+		{
+			// FIXME: Report an error
+		}
+	}
+}
+
+function inventory_change(item_id)
+{
+	var data = new Array();
+	data[inventory_parameters] = item_id;
+	post_action_data(inventory_action, data);
+	var select_panel = document.getElementById("inventory_panel");
+	select_panel.visible = false;
+}
