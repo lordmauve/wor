59129553642c64a106515dea613db018967a2b5d
hrm <hrm@localhost>
2009-07-13 14:41:49 +0000

    Add range-setting for plot

--- tools/scape/workflow.sh
+++ tools/scape/workflow.sh
@@ -8,4 +8,6 @@
 ./loc-fractal -c cragginess.txt -k scaffold-main.txt >height.txt
 ./hydro -r new-height.txt -w new-water.txt -H height.txt
 
+for f in water.*.txt; do ./plot -o ${f/txt/png} -f $f -0 -m 128,128,255; done
+
 ./plot -o water.png -f new-water.txt -0 -m 128,128,255
