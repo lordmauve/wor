6b86806aafe8cd4167f40f484395c8d306b16aeb
hrm <hrm@localhost>
2009-07-17 23:16:11 +0000

    Fiddling with landscape generation

--- tools/scape/workflow.sh
+++ tools/scape/workflow.sh
@@ -10,12 +10,14 @@
 
 . <(./minmax water.*.txt)
 for f in water.*.txt; do
-	./plot -o ${f/txt/png} --min-max $min,$max -f $f -0 -m 128,128,255
+	./plot -o ${f/txt/png} -f $f --min-max $min,$max -m 128,128,255 -l
 done
+ffmpeg -r 4 -i water.%03d.png -r 25 water.mpg
 
 . <(./minmax height.*.txt)
 for f in height.*.txt; do
-	./plot -o ${f/txt/png} --min-max $min,$max -f $f -0 -m 255,255,255
+	./plot -o ${f/txt/png} -f $f --min-max $min,$max -m 255,255,255
 done
+ffmpeg -r 4 -i height.%03d.png -r 25 height.mpg
 
 ./plot -o water.png -f new-water.txt -0 -m 128,128,255
