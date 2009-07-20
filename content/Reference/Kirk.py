f7530cccd2d68aa9026f8f387cef76502bf4dbbc
dbrinker <dbrinker@localhost>
2009-07-20 04:19:54 +0000

    Added kirk generation (TODO: make it callable from code)

---
+++ content/Reference/Kirk.py
@@ -0,0 +1,26 @@
+####
+# Reference data for kirk generation.  Feel free to add data here
+
+adjective1s = ["",
+    "Rampant",
+    "Reformed", 
+    "Reconstructed", 
+    "Revised", 
+    "Revisited", 
+    "Recidivist", 
+    "Resurrected"]
+
+adjective2s = [
+    "Revealed", 
+    "Returned", 
+    "Released", 
+    "Triumphant"]
+    
+options = ["",
+    "True",
+    "Honest",
+    "Righteous",
+    "Upright",
+    "Eternal",
+    "Inconceivable",
+    "Incorruptible"]    
---
+++ content/Reference/Ordinal.py
@@ -0,0 +1,13 @@
+####
+# Ordinal names.  Feel free to extend this further if 1-9 isn't enough for you
+
+ordinals = [
+    "First", 
+    "Second", 
+    "Third", 
+    "Fourth", 
+    "Fifth", 
+    "Sixth", 
+    "Seventh", 
+    "Eighth", 
+    "Ninth"]
