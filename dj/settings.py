74f7983a17404173ae3551838125ebd4bbb8e139
Daniel Pope <lordmauve@users.noreply.github.com>
2009-11-30 22:00:47 +0000

    A few tidy-ups to make everything work out of the box:
    Fix: Missing dj/editor/__init__.py from earlier commit
    New: Automatically generate BaseConfig.py
    Fix: Make tools/shell executable

--- dj/settings.py
+++ dj/settings.py
@@ -9,13 +9,7 @@ ADMINS = [
 
 MANAGERS = ADMINS
 
-import DBAuth
-DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = DBAuth.name             # Or path to database file if using sqlite3.
-DATABASE_USER = DBAuth.user             # Not used with sqlite3.
-DATABASE_PASSWORD = DBAuth.passwd         # Not used with sqlite3.
-DATABASE_HOST = DBAuth.host             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = ''           # No Django database - using ZODB instead
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -32,16 +26,16 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '../server_root/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
---
+++ library/BaseConfig.py
@@ -0,0 +1,6 @@
+"""Path configuration"""
+import os
+import os.path
+
+base_dir = os.path.join(os.path.dirname(__file__), '..')
+log_dir = os.path.join(base_dir, "logs")
