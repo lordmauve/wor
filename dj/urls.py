0733f3c0a9ebfaa77d64b448ac846478e4ec297c
mauve <mauve@localhost>
2009-11-30 20:48:41 +0000

    Fix for previous commit where contents of some files was duplicated

--- dj/urls.py
+++ dj/urls.py
@@ -21,20 +21,3 @@ urlpatterns = patterns('api_views',
 urlpatterns += patterns('', 
 	(r'(.*)', 'django.views.static.serve', {'document_root': '../server_root/', 'show_indexes': True}),
 )
-from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^dj/', include('dj.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-)
