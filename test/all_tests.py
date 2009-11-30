e59df2983d7990bcc2dce61f70042785e9752d88
hrm <hrm@localhost>
2009-11-30 12:39:47 +0000

    Test harness for Strategy classes.
    This is the stand-alone unit tests for the Strategy class code.  The
    require no additional parts of the game code.

--- test/all_tests.py
+++ test/all_tests.py
@@ -4,9 +4,11 @@ import unittest
 
 import Serialisation
 import TestItemContainer
+import TestStrategy
 
 s = unittest.TestSuite()
 s.addTest(Serialisation.suite())
 s.addTest(TestItemContainer.suite())
+s.addTest(TestStrategy.suite())
 
 unittest.TextTestRunner(verbosity=2).run(s)
