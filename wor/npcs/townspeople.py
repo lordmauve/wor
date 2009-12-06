8fd874e33abc4df9f558da4393c289836659b019
Daniel Pope <lordmauve@users.noreply.github.com>
2009-12-06 14:00:07 +0000

    Refactor: extracted Barmaid seller behaviour to a mix-in class Seller New: drinks.Tequila - sometimes with a worm Merge: Merging Bayes generator from trunk

--- wor/npcs/townspeople.py
+++ wor/npcs/townspeople.py
@@ -1,13 +1,12 @@
 from wor.actors.npc import *
-from wor.actions import trading
 
 
-class Barmaid(HumanFemaleNPC):
+class Barmaid(Seller, HumanFemaleNPC):
 	taxonomy = 'human.female.Barmaid'
 	short_name = 'the Barmaid'
 	full_name_format = '%s, the barmaid'
 
-	def external_actions(self, player):
-		return [
-			trading.ActionBuy(player, seller=self, item='drinks.Ale', price=3)
-		]
+	sells = [
+		('drinks.Ale', 3),
+		('drinks.Tequila', 2),
+	]
