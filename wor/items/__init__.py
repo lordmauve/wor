bca344f9ad33c6fa335f6fa8aee2ec1538992919
Daniel Pope <dan@mauveinternet.co.uk>
2012-02-28 07:51:28 +0000

    New: foraging

--- wor/items/__init__.py
+++ wor/items/__init__.py
@@ -2,3 +2,4 @@ from martial import *
 from currency import *
 from drinks import *
 from weapons import *
+from natural import *
---
+++ wor/items/natural.py
@@ -0,0 +1,14 @@
+from base import AggregateItem
+
+class Twig(AggregateItem):
+    name = 'a twig'
+    name_plural = '%d twigs'
+
+    group = "Miscellaneous"
+
+
+class Stick(AggregateItem):
+    name = 'a stick'
+    name_plural = '%d sticks'
+
+    group = "Miscellaneous"
